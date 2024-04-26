2b0bb555dc392727fdee8221bea13d67209cd733
Subashk129 <subashksbs@gmail.com>
2024-04-26 19:58:43 +0530

    first commit

---
+++ Abstraction/Principal.java
@@ -0,0 +1,16 @@
+package Abstraction;
+
+public abstract class Principal {
+    
+    public abstract void teachersandengage();
+
+    public abstract void scduleptm();
+
+    public abstract void conductsports();
+
+    public void delivarywelcomeaddress(){
+      
+       System.out.println("The Principal is responciple to deliver Welcome Address");
+    }
+    
+}
